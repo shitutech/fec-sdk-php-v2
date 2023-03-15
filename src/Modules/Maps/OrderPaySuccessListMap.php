c8e995404355df901f3f9c9893ba573c3c41fcb7
greenrock <github@miss77.net>
2023-03-15 20:15:54 +0800

    订单支付接口

---
+++ src/Modules/Maps/OrderPaySuccessListMap.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Shitutech\Fes\Modules\Maps;
+
+use Shitutech\Fes\Modules\Base\BaseObject;
+
+class OrderPaySuccessListMap extends BaseObject
+{
+    /**
+     * @var string 系统会员ID
+     */
+    protected $systemId = '';
+
+    /**
+     * @var string 会员名称
+     */
+    protected $name = '';
+
+    /**
+     * @var string 身份证号
+     */
+    protected $idCard = '';
+
+    /**
+     * @var string 开户行编号
+     */
+    protected $bankNo = '';
+
+    /**
+     * @var string 开户行名称
+     */
+    protected $bankName = '';
+
+    /**
+     * @var string 结算卡号
+     */
+    protected $cardNo = '';
+
+    /**
+     * @var string 结算卡号银行预留手机号
+     */
+    protected $bankPhone = '';
+
+    /**
+     * @var string 金额
+     */
+    protected $fee = '';
+
+    /**
+     * @var string 交易流水号
+     */
+    protected $orderNo = '';
+
+    /**
+     * @var string 支付状态[0:待支付 1:支付中 2:支付成功 3:支付失败4:已撤回]
+     */
+    protected $payStatus = '';
+
+    /**
+     * @return string
+     */
+    public function getSystemId(): string
+    {
+        return $this->systemId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdCard(): string
+    {
+        return $this->idCard;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBankNo(): string
+    {
+        return $this->bankNo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBankName(): string
+    {
+        return $this->bankName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCardNo(): string
+    {
+        return $this->cardNo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBankPhone(): string
+    {
+        return $this->bankPhone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFee(): string
+    {
+        return $this->fee;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrderNo(): string
+    {
+        return $this->orderNo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPayStatus(): string
+    {
+        return $this->payStatus;
+    }
+
+}
